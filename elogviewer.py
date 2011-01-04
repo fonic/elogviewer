c968ea0ab06ab1ee0fce2f16a2061d00a62e7c55
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2011-01-04 12:45:34 +0100

    Refactor EvGtk::main into ev.py::main
    Copy calls to functions creating and launching the GUI from
    elogviewerGtk::main to elogviewer.py. The GUI launched is selected by
    the cmdline argument created in the previous commit

--- elogviewer.py
+++ elogviewer.py
@@ -5,10 +5,44 @@
 # see elogviewerCommon.py for details
 
 import sys
-from elogviewerCommon import CommandLineArguments
+from elogviewerCommon import parseArguments
 def main(argv):
 	cmdline = parseArguments(argv)
 
-if __name__ = "__main__":
+	if cmdline.get_gui_frontend() == "QT":
+		from elogviewerQt import ElogviewerQt as ElogviewerGui
+		from elogviewerQt import Filter
+	else:
+		from elogviewerGtk import ElogviewerGtk as ElogviewerGui
+		from elogviewerGtk import Filter
+	
+	elogviewer = ElogviewerGui(cmdline)
+	elogviewer.create_gui()
+
+	elogviewer.add_filter(Filter("info", "INFO", True, 'darkgreen'))
+	elogviewer.add_filter(Filter("warning", "WARN", True, 'red'))
+	elogviewer.add_filter(Filter("error", "ERROR", True, 'orange'))
+	elogviewer.add_filter(Filter("log", "LOG", True))
+	elogviewer.add_filter(Filter("QA", "QA", True))
+
+	elogviewer.add_filter(Filter("preinst"))
+	elogviewer.add_filter(Filter("postinst"))
+	elogviewer.add_filter(Filter("prerm"))
+	elogviewer.add_filter(Filter("postrm"))
+	elogviewer.add_filter(Filter("unpack"))
+	elogviewer.add_filter(Filter("compile"))
+	elogviewer.add_filter(Filter("setup"))
+	elogviewer.add_filter(Filter("test"))
+	elogviewer.add_filter(Filter("install"))
+	elogviewer.add_filter(Filter("prepare"))
+	elogviewer.add_filter(Filter("configure"))
+	elogviewer.add_filter(Filter("other"))
+
+	elogviewer.connect()
+	elogviewer.show()
+	elogviewer.refresh()
+	elogviewer.main()
+
+if __name__ == "__main__":
 	main(sys.argv[1:])
 
