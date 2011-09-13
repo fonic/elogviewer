6fcaf6236c773b3e5f441864b590ce0c2dfaee1a
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2011-09-13 21:33:01 +0200

    per package instead of per module

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(name="elogviewer",
       author_email="Mathias.Laurin+gentoo.org@gmail.com",
       url="http://sourceforge.net/projects/elogviewer/",
       license="GPL2",
-      #py_module=["libelogviewer"],
       #packages_dir={"": "libelogviewer"},
       packages=["libelogviewer", "libelogviewer/ev_gtk", "libelogviewer/ev_qt"],
       package_data={"": ["libelogviewer/rsc/qt.rsc"]}
