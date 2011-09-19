66807189e97e49ce11dc2d4c7ed0affcf382f355
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2011-09-19 20:05:37 +0000

    add glade file

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ Usage:
 from distutils.core import setup
 
 setup(name="elogviewer",
-      version="1.0.0",
+      version="1.0.1",
       author="Mathias Laurin",
       author_email="Mathias.Laurin+gentoo.org@gmail.com",
       url="http://sourceforge.net/projects/elogviewer/",
       license="GPL2",
       packages=["libelogviewer", "libelogviewer/ev_gtk", "libelogviewer/ev_qt"],
-      #package_data={"libelogviewer": ["libelogviewer/rsc/qt.qrc"]},
+      package_data={"libelogviewer": ["libelogviewer/rsc/qt.qrc"],
+                    "libelogviewer/ev_gtk": ["libelogviewer/ev_gtk/elogviewer.glade"]
+                    },
+	  #data_files=[('libelogviewer/ev_gtk', ['libelogviewer/ev_gtk/elogviewer.glade']),],
       scripts=["elogviewer.py"],
      )
