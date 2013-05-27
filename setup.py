e6bd37c0b53a9cc485bb74b92684783902da6c01
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2013-05-27 23:32:14 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 """
 Usage:
-    python setup.py
+    python setup.py sdist
 """
 
+import os
 from distutils.core import setup
+import elogviewer
 
 setup(name="elogviewer",
-      version="1.0.1",
+      version=elogviewer.__version__,
       author="Mathias Laurin",
       author_email="Mathias.Laurin+gentoo.org@gmail.com",
       url="http://sourceforge.net/projects/elogviewer/",
-      license="GPL2",
-      packages=["libelogviewer", "libelogviewer/ev_gtk", "libelogviewer/ev_qt"],
-      package_data={"libelogviewer": ["libelogviewer/rsc/qt.qrc"],
-                    "libelogviewer/ev_gtk": ["libelogviewer/ev_gtk/elogviewer.glade"]
-                    },
-	  #data_files=[('libelogviewer/ev_gtk', ['libelogviewer/ev_gtk/elogviewer.glade']),],
+      license="GPLv2",
+      data_files=[("", ["elogviewer.1", "LICENSE.TXT"])],
       scripts=["elogviewer.py"],
+      classifiers=os.linesep.join(
+          s for s in """
+          Development Status :: 4 - Beta
+          License :: OSI Approved :: GNU General Public License v2 (GPLv2)
+          Programming Language :: Python :: 3
+          Environment :: X11 Applications :: Qt
+          """)
      )
