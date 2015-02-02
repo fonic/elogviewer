80ffc098b52d92516eb36e7ed1890f9d90922ed5
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2015-02-02 20:11:58 +0000

    Unittest html output

---
+++ tests.py
@@ -0,0 +1,29 @@
+from elogviewer import Elog
+import unittest
+from glob import glob
+from os import path
+
+
+class TestElog(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = None
+        datadir = "./data"
+        self.elogs = glob(path.join(datadir, "*.log"))
+        self.htmls = [".".join((path.splitext(elog)[0], "html"))
+                      for elog in self.elogs]
+        for elog, html in zip(self.elogs, self.htmls):
+            if not path.isfile(html):
+                with open(html, "w") as html_file:
+                    html_file.writelines(Elog(elog).htmltext)
+
+    def test_html_parser(self):
+        for elog, html in zip(self.elogs, self.htmls):
+            with open(html, "r") as html_file:
+                self.assertMultiLineEqual(
+                    Elog(elog).htmltext,
+                    "".join(html_file.readlines()))
+
+
+if __name__ == "__main__":
+    unittest.main()
